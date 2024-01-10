b02493047fcc1bf0eeacf2cc9effa916be00217c
ansarimati <ansarimati.aiktc@gmail.com>
2024-01-10 23:52:36 +0530

    Auth wip

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,8 +1,20 @@
 
 const Register = () => {
     return (
-        <section className="text-center text-primary text-4xl mt-8 font-semibold">
-            Register
+        <section className="mt-8">
+            <h1 className="text-center text-primary text-4xl font-semibold">
+                Register
+            </h1>
+
+            <form className="block max-w-xl mx-auto">
+                <input type="email" placeholder="email" />
+                <input type="password" placeholder="password" />
+                <button type="submit">Register</button>
+            </form>
+
+            <div>
+                <button>Login with google</button>
+            </div>
         </section>
     )
 };
